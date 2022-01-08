0a45506f22bfcdbbda5045a4164675ace83ac7de
jpalmer <j_palmeros@yahoo.com.mx>
2022-01-08 05:49:58 -0600

    Terminamos el primer capitulo

---
+++ Chapter01/chapter1_01.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.btn = tk.Button(self, text="Click Me!",
+                            command=self.say_hello)
+        self.btn.pack(padx=120, pady=30)
+    
+    def say_hello(self):
+        print("Hello, Tkinter!")
+
+if __name__ == "__main__":
+    app = App()
+    app.title("My Tkinter app")
+    app.mainloop()
+    
